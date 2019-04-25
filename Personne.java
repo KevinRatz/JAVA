6ff9560416069ef0039085615ef4fe03fdd1ec7f
Maxime3f <35740668+Maxime3f@users.noreply.github.com>
2019-04-25 14:53:36 +0200

    Add files via upload

---
+++ Personne.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applic_salle_presse;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Personne implements Identifiable {
+    private String _nom;
+    private String _prenom;
+    private String _numCI;
+    
+    public String getNom() {
+        return _nom;
+    }
+    public void setNom(String nom) {
+        this._nom = nom;
+    }
+    public String getPrenom() {
+        return _prenom;
+    }
+    public void setPrenom(String prenom) {
+        this._prenom = prenom;
+    }
+    public String getNumCI() {
+        return _numCI;
+    }
+    public void setNumCI(String numCI) {
+        this._numCI = numCI;
+    }
+}
